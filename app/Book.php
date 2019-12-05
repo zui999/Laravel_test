c13094cb19238a9346ba45b013596305b0be4ff2
Ittei Zui <zui@fez-inc.jp>
2019-12-05 18:00:22 +0900

    Fix Book model file about relation info

--- app/Book.php
+++ app/Book.php
@@ -7,9 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     public $table = 'books';
-
-    public function shopcarts()
-    {
-         return $this->belongsToMany('App\ShopCart')->using('App\ShopCartItems');
-    }
 }
