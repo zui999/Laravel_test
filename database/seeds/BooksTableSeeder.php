82cacc7a43ba68c7810edfc4751d13c7f4dcad4c
Ittei Zui <zui@fez-inc.jp>
2019-12-05 16:26:11 +0900

    Update Book database info

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -12,5 +12,25 @@ class BooksTableSeeder extends Seeder
     public function run()
     {
         //
+        // テーブルのクリア
+       DB::table('books')->truncate();
+
+        // 初期データ用意（列名をキーとする連想配列）
+        $books = [
+                  ['name' => 'PHP Book',
+                  'price' => 2000,
+                   'author' => 'PHPER'],
+                  ['name' => 'Laravel Book',
+                   'price' => 3000,
+                   'author' => null],
+                  ['name' => 'Ruby Book',
+                   'price' => 2500,
+                   'author' => 'Rubyist']
+                 ];
+
+        // 登録
+        foreach($books as $book) {
+          \App\Book::create($book);
+        }
     }
 }
