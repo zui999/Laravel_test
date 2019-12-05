c4b0dac3078b82a23036fd21fc05dd0222e38fc8
Ittei Zui <zui@fez-inc.jp>
2019-12-05 16:28:52 +0900

    Fix Book model

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    public $table = 'books';
+
+    public function shopcarts()
+    {
+         return $this->belongsToMany('App\ShopCart')->using('App\ShopCartItems');
+    }
 }
